ee909e411e5a31c940a5f79e9d13408c8b3c6cbb
Vipul Sharma <vipulsharma018@gmail.com>
2019-12-16 23:35:37 -0500

    postgre database

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -1,48 +1,48 @@
 const getTableData = (req, res, db) => {
-  db.select('*').from('testtable1')
-    .then(items => {
-      if(items.length){
-        res.json(items)
-      } else {
-        res.json({dataExists: 'false'})
-      }
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+    console.log("GET");
+    db.select('*').from('subscription')
+        .then(items => {
+            if (items.length) {
+                res.json(items)
+            } else {
+                res.json({dataExists: 'false'})
+            }
+        })
+        .catch(err => res.status(400).json({dbError: 'db error'}))
+};
 
 const postTableData = (req, res, db) => {
-  const { first, last, email, phone, location, hobby } = req.body
-  const added = new Date()
-  db('testtable1').insert({first, last, email, phone, location, hobby, added})
-    .returning('*')
-    .then(item => {
-      res.json(item)
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+    const {email, city, longitude, latitude} = req.body
+    db('subscription').insert({email, city, longitude, latitude})
+        .returning('*')
+        .then(item => {
+            res.json(item)
+        })
+        .catch(err => res.status(400).json({dbError: 'db error'}))
+};
 
 const putTableData = (req, res, db) => {
-  const { id, first, last, email, phone, location, hobby } = req.body
-  db('testtable1').where({id}).update({first, last, email, phone, location, hobby})
-    .returning('*')
-    .then(item => {
-      res.json(item)
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+    const {id, email, city, longitude, latitude} = req.body;
+    db('subscription').where({id}).update({email, city, longitude, latitude})
+        .returning('*')
+        .then(item => {
+            res.json(item)
+        })
+        .catch(err => res.status(400).json({dbError: 'db error'}))
+};
 
 const deleteTableData = (req, res, db) => {
-  const { id } = req.body
-  db('testtable1').where({id}).del()
-    .then(() => {
-      res.json({delete: 'true'})
-    })
-    .catch(err => res.status(400).json({dbError: 'db error'}))
-}
+    const {id} = req.body;
+    db('subscription').where({id}).del()
+        .then(() => {
+            res.json({delete: 'true'})
+        })
+        .catch(err => res.status(400).json({dbError: 'db error'}))
+};
 
 module.exports = {
-  getTableData,
-  postTableData,
-  putTableData,
-  deleteTableData
-}
+    getTableData,
+    postTableData,
+    putTableData,
+    deleteTableData
+};
