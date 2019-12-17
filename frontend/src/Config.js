89bdfa678eab60bab738f8fc2368d84dcdb8e44e
Vipul Sharma <vipulsharma018@gmail.com>
2019-12-17 04:07:15 -0500

    key removed

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,13 +1,1 @@
-// Your web app's Firebase configuration
-const DB_CONFIG = {
-    apiKey: "AIzaSyBpfjGZTlNWo9rK8yA1c4N_Dd9dB-PfRFg",
-    authDomain: "klaviyo-weather-app.firebaseapp.com",
-    databaseURL: "https://klaviyo-weather-app.firebaseio.com",
-    projectId: "klaviyo-weather-app",
-    storageBucket: "klaviyo-weather-app.appspot.com",
-    messagingSenderId: "1049596033882",
-    appId: "1:1049596033882:web:e8a7a4a650e3ec78841163",
-    measurementId: "G-LHM0QBSNNB"
-};
 
-export default DB_CONFIG;
